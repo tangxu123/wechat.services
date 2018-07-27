150365a1425a21a635733c16e1a3b646263dd80d
him <himkwok@126.com>
2018-07-27 11:30:09 +0800

    徐汇资产管理

---
+++ wechat.service/src/main/java/com/ludateam/wechat/entity/ZcrkXlb.java
@@ -0,0 +1,277 @@
+package com.ludateam.wechat.entity;
+
+/**
+ * Created by lenovo on 2018/7/27.
+ */
+public class ZcrkXlb {
+    private String wyid;
+    private String zcbh;
+    private String xxhfl;
+    private String bqlx;
+    private String zcbh_ys;
+    private String bmmc;
+    private String syr;
+    private String zcxl_dm;
+    private String zcxlmc;
+    private String zcmc;
+    private String pp;
+    private String ggxh;
+    private String jgzb;
+    private String cfck;
+    private String jqbh;
+    private String rksj;
+    private String zt;
+    private String wl;
+    private String ip;
+    private String wlbh;
+    private String wzm;
+    private String syyt;
+    private String syqk;
+    private String dwmc;
+    private String dwbh;
+    private String rkdj;
+    private String bz1;
+    private String bz2;
+    private String wz_bz;
+    private String syzt;
+
+    public String getWyid() {
+        return wyid;
+    }
+
+    public void setWyid(String wyid) {
+        this.wyid = wyid;
+    }
+
+    public String getZcbh() {
+        return zcbh;
+    }
+
+    public void setZcbh(String zcbh) {
+        this.zcbh = zcbh;
+    }
+
+    public String getXxhfl() {
+        return xxhfl;
+    }
+
+    public void setXxhfl(String xxhfl) {
+        this.xxhfl = xxhfl;
+    }
+
+    public String getBqlx() {
+        return bqlx;
+    }
+
+    public void setBqlx(String bqlx) {
+        this.bqlx = bqlx;
+    }
+
+    public String getZcbh_ys() {
+        return zcbh_ys;
+    }
+
+    public void setZcbh_ys(String zcbh_ys) {
+        this.zcbh_ys = zcbh_ys;
+    }
+
+    public String getBmmc() {
+        return bmmc;
+    }
+
+    public void setBmmc(String bmmc) {
+        this.bmmc = bmmc;
+    }
+
+    public String getSyr() {
+        return syr;
+    }
+
+    public void setSyr(String syr) {
+        this.syr = syr;
+    }
+
+    public String getZcxl_dm() {
+        return zcxl_dm;
+    }
+
+    public void setZcxl_dm(String zcxl_dm) {
+        this.zcxl_dm = zcxl_dm;
+    }
+
+    public String getZcxlmc() {
+        return zcxlmc;
+    }
+
+    public void setZcxlmc(String zcxlmc) {
+        this.zcxlmc = zcxlmc;
+    }
+
+    public String getZcmc() {
+        return zcmc;
+    }
+
+    public void setZcmc(String zcmc) {
+        this.zcmc = zcmc;
+    }
+
+    public String getPp() {
+        return pp;
+    }
+
+    public void setPp(String pp) {
+        this.pp = pp;
+    }
+
+    public String getGgxh() {
+        return ggxh;
+    }
+
+    public void setGgxh(String ggxh) {
+        this.ggxh = ggxh;
+    }
+
+    public String getJgzb() {
+        return jgzb;
+    }
+
+    public void setJgzb(String jgzb) {
+        this.jgzb = jgzb;
+    }
+
+    public String getCfck() {
+        return cfck;
+    }
+
+    public void setCfck(String cfck) {
+        this.cfck = cfck;
+    }
+
+    public String getJqbh() {
+        return jqbh;
+    }
+
+    public void setJqbh(String jqbh) {
+        this.jqbh = jqbh;
+    }
+
+    public String getRksj() {
+        return rksj;
+    }
+
+    public void setRksj(String rksj) {
+        this.rksj = rksj;
+    }
+
+    public String getZt() {
+        return zt;
+    }
+
+    public void setZt(String zt) {
+        this.zt = zt;
+    }
+
+    public String getWl() {
+        return wl;
+    }
+
+    public void setWl(String wl) {
+        this.wl = wl;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getWlbh() {
+        return wlbh;
+    }
+
+    public void setWlbh(String wlbh) {
+        this.wlbh = wlbh;
+    }
+
+    public String getWzm() {
+        return wzm;
+    }
+
+    public void setWzm(String wzm) {
+        this.wzm = wzm;
+    }
+
+    public String getSyyt() {
+        return syyt;
+    }
+
+    public void setSyyt(String syyt) {
+        this.syyt = syyt;
+    }
+
+    public String getSyqk() {
+        return syqk;
+    }
+
+    public void setSyqk(String syqk) {
+        this.syqk = syqk;
+    }
+
+    public String getDwmc() {
+        return dwmc;
+    }
+
+    public void setDwmc(String dwmc) {
+        this.dwmc = dwmc;
+    }
+
+    public String getDwbh() {
+        return dwbh;
+    }
+
+    public void setDwbh(String dwbh) {
+        this.dwbh = dwbh;
+    }
+
+    public String getRkdj() {
+        return rkdj;
+    }
+
+    public void setRkdj(String rkdj) {
+        this.rkdj = rkdj;
+    }
+
+    public String getBz1() {
+        return bz1;
+    }
+
+    public void setBz1(String bz1) {
+        this.bz1 = bz1;
+    }
+
+    public String getBz2() {
+        return bz2;
+    }
+
+    public void setBz2(String bz2) {
+        this.bz2 = bz2;
+    }
+
+    public String getWz_bz() {
+        return wz_bz;
+    }
+
+    public void setWz_bz(String wz_bz) {
+        this.wz_bz = wz_bz;
+    }
+
+    public String getSyzt() {
+        return syzt;
+    }
+
+    public void setSyzt(String syzt) {
+        this.syzt = syzt;
+    }
+}
